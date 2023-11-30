7f3413ff262c23ff3ccaaf612828db2d24664020
Mark <mark130599@gmail.com>
2023-11-30 16:32:51 +0200

    revert commit "fix bottom sheet bug: ending up behind system navigation"

--- app/src/main/java/com/example/happystudent/app/MainActivity.kt
+++ app/src/main/java/com/example/happystudent/app/MainActivity.kt
@@ -3,14 +3,6 @@ package com.example.happystudent.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.view.WindowCompat
 import com.example.happystudent.core.theme.HappyStudentTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,10 +11,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // We handle all the insets manually
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-
         setContent {
             HappyStudentTheme(dynamicColor = false) {
                 // A surface container using the 'background' color from the theme
--- app/src/main/java/com/example/happystudent/feature/students/StudentListScreen.kt
+++ app/src/main/java/com/example/happystudent/feature/students/StudentListScreen.kt
@@ -9,11 +9,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -143,20 +140,16 @@ fun FilterBottomSheet(
     onChangePriority: (String) -> Unit
 ) {
 
-    val bottomPadding = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
-
     if (showBottomState) {
         ModalBottomSheet(
             onDismissRequest = { onShowChange(false) },
             sheetState = sheetState
         ) {
-            Column(
-                modifier = Modifier.padding(bottom = bottomPadding)
-            ) {
-                PriorityChips(
-                    onChangePriority = onChangePriority
-                )
-            }
+
+            PriorityChips(
+                onChangePriority = onChangePriority
+            )
+
 
         }
     }
