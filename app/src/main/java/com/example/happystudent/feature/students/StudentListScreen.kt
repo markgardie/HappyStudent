7422eec2c44095f51f3d7fb17b05e88d0aef1c2d
Mark <mark130599@gmail.com>
2023-12-19 16:03:58 +0200

    show student photo in student list

--- app/src/main/java/com/example/happystudent/feature/students/StudentListScreen.kt
+++ app/src/main/java/com/example/happystudent/feature/students/StudentListScreen.kt
@@ -2,6 +2,7 @@ package com.example.happystudent.feature.students
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.fadeOut
@@ -14,9 +15,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
@@ -50,10 +53,13 @@ import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.AsyncImage
 import com.example.happystudent.R
 import com.example.happystudent.core.datastore.FilterPreferences.FilterType
 import com.example.happystudent.core.model.Student
@@ -421,9 +427,14 @@ fun StudentCard(
 
         },
         leadingContent = {
-            Icon(
-                imageVector = Icons.Filled.Person,
-                contentDescription = "Фото студента"
+            AsyncImage(
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape),
+                model = Uri.parse(student.imageUri),
+                error = painterResource(id = R.drawable.avatar_placeholder),
+                contentDescription = "Фото студента",
+                contentScale = ContentScale.Crop
             )
         },
         trailingContent = {
