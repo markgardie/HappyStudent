aca02a7a1b83e6294d3d9aec3fe8ad462e0628c0
Mark <mark130599@gmail.com>
2023-12-20 14:14:47 +0200

    delete StudentGroup.kt class

--- app/src/main/java/com/example/happystudent/core/database/model/StudentGroup.kt
+++
@@ -1,8 +0,0 @@
-package com.example.happystudent.core.database.model
-
-import com.example.happystudent.core.model.Student
-
-data class StudentGroup(
-    val name: String,
-    val students: List<Student>
-)
